710bb49a584c66482cbce8a31aa0d41d71d414f6
Rezaul Karim RK <55434814+rezaulkarimrk@users.noreply.github.com>
2023-03-02 09:42:30 -0800

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+(function ($) {
+	"use strict";
+	jQuery(document).ready(function(){
+
+		$('.testmonial-carousel').owlCarousel({
+			items: 1,
+			autoplay:true,
+			loop: true,
+			nav: false,
+			dots:true,
+			// navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+			// smartSpeed: 2000,
+			// navSpeed: 2000,
+			 dotsSpeed: 2000,
+			autoplaySpeed: 3000,
+		});
+		
+
+
+	});
+
+
+
+
+	jQuery(window).load(function(){
+
+
+		
+	});
+
+
+
+}(jQuery));
+
